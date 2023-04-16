fd90478cb3f26c1af3e6faee40842073079842d9
Le Duc Tho <leductho911@gmail.com>
2023-04-16 20:21:25 +0700

    Added BasePage class to the actions/commons package Added client-combined-3.141.59-sources.jar to the libraries folder Created three test cases in the testcases/com/nopcommerce/user package: Level_02_Apply_BasePage_1 Level_02_Apply_BasePage_2 Level_02_Apply_BasePage_3 Added Template.java to the javaTester/eclipseTips directory

---
+++ javaTester/eclipseTips/Template.java
@@ -0,0 +1,12 @@
+package eclipseTips;
+
+import org.openqa.selenium.WebDriver;
+
+public class Template {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+
+	}
+
+}
---
+++ testcases/com/nopcommerce/user/Level_02_Apply_BasePage_3.java
@@ -0,0 +1,149 @@
+// Framework 04 - Apply BasePage/ NopCommerce Checklist Business
+// Note : @BeforeClass and AfterClass khong xu ly trong bai nay https://youtu.be/F-3M9oLKEXE?t=582
+
+package com.nopcommerce.user;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+//comment4
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import commons.BasePage;
+
+public class Level_02_Apply_BasePage_3 extends BasePage {
+
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String emailAddress;
+	JavascriptExecutor jsExecutor;
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		driver = new FirefoxDriver();
+		jsExecutor = (JavascriptExecutor) driver;
+		emailAddress = "leductho" + randomNumber() + "@gmail.com";
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://demo.nopcommerce.com/");
+
+	}
+
+	@Test
+	public void TC_01_Register_Empty_Data() {
+		waitForElementClickable(driver, "//a[@class='ico-register']");
+		clickToElement(driver, "//a[@class='ico-register']");
+		// driver.findElement(By.cssSelector("a.ico-register")).click();
+
+		waitForElementClickable(driver, "//button[@id='register-button']");
+		clickToElement(driver, "//button[@id='register-button']");
+		// driver.findElement(By.cssSelector("button#register-button")).click();
+
+		Assert.assertEquals(getElementText(driver, "//span[@id='FirstName-error']"), "First name is required.");
+		// Assert.assertEquals(driver.findElement(By.cssSelector("#FirstName-error")).getText(),
+		// "First name is required.");
+
+	}
+
+	@Test
+	public void TC_02_Register_Invalid_Email() {
+		waitForElementClickable(driver, "//a[@class='ico-register']");
+		clickToElement(driver, "//a[@class='ico-register']");
+
+		sendKeysToElement(driver, "//input[@id='Email']", "dsaf4gmail.com");
+		// driver.findElement(By.cssSelector("input#Email")).sendKeys("dsaf4gmail.com");
+
+		waitForElementClickable(driver, "//button[@id='register-button']");
+		clickToElement(driver, "//button[@id='register-button']");
+
+		Assert.assertEquals(getElementText(driver, "//span[@id='Email-error']"), "Wrong email");
+		// Assert.assertEquals(driver.findElement(By.cssSelector("span#Email-error")).getText(),
+		// "Wrong email");
+
+	}
+
+	@Test
+	public void TC_03_Register_Success() {
+		waitForElementClickable(driver, "//a[@class='ico-register']");
+		clickToElement(driver, "//a[@class='ico-register']");
+
+		sendKeysToElement(driver, "//input[@id='FirstName']", "Automation");
+		sendKeysToElement(driver, "//input[@id='LastName']", "FC");
+		sendKeysToElement(driver, "//input[@id='Email']", emailAddress);
+		sendKeysToElement(driver, "//input[@id='Password']", "123456");
+		sendKeysToElement(driver, "//input[@id='ConfirmPassword']", "123456");
+		clickToElement(driver, "//input[@id='gender-male']");
+
+		clickToElement(driver, "//button[@id='register-button']");
+
+		Assert.assertEquals(getElementText(driver, "//div[@class='result']"), "Your registration completed");
+
+	}
+
+	@Test
+	public void TC_04_Register_Exiting_Email() {
+		waitForElementClickable(driver, "//a[@class='ico-register']");
+		clickToElement(driver, "//a[@class='ico-register']");
+
+		sendKeysToElement(driver, "//input[@id='FirstName']", "Automation");
+		sendKeysToElement(driver, "//input[@id='LastName']", "FC");
+		sendKeysToElement(driver, "//input[@id='Email']", emailAddress);
+		sendKeysToElement(driver, "//input[@id='Password']", "123456");
+		sendKeysToElement(driver, "//input[@id='ConfirmPassword']", "123456");
+		clickToElement(driver, "//input[@id='gender-male']");
+
+		clickToElement(driver, "//button[@id='register-button']");
+
+		Assert.assertTrue(isElementDisplayed(driver, "//li[normalize-space()='The specified email already exists']"));
+
+	}
+
+	@Test
+	public void TC_05_Register_Password_Less_Than_6_Chars() {
+		waitForElementClickable(driver, "//a[@class='ico-register']");
+		clickToElement(driver, "//a[@class='ico-register']");
+
+		sendKeysToElement(driver, "//input[@id='Password']", "12345");
+		clickToElement(driver, "//button[@id='register-button']");
+
+		Assert.assertEquals(getElementText(driver, "//span[@id='Password-error']"),
+				"Password must meet the following rules:\nmust have at least 6 characters");
+
+		// Assert.assertEquals(driver.findElement(By.cssSelector("span#Password-error")).getText(),
+		// "Password must meet the following rules:\nmust have at least 6 characters");
+	}
+
+	@Test
+	public void TC_06_Register_Invalid_Confirm_Password() {
+		waitForElementClickable(driver, "//a[@class='ico-register']");
+		clickToElement(driver, "//a[@class='ico-register']");
+		sendKeysToElement(driver, "//input[@id='Password']", "123456");
+		sendKeysToElement(driver, "//input[@id='ConfirmPassword']", "1234567");
+		clickToElement(driver, "//button[@id='register-button']");
+
+		Assert.assertEquals(getElementText(driver, "//span[@id='ConfirmPassword-error']"),
+				"The password and confirmation password do not match.");
+
+		// Assert.assertEquals(driver.findElement(By.cssSelector("span#ConfirmPassword-error")).getText(),
+		// "The password and confirmation password do not match.");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	public int randomNumber() {
+		Random rand = new Random();
+		return rand.nextInt(9999);
+
+	}
+
+}
